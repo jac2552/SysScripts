1a1cfe4d928d7bf493474062f95d5af47d98eacb
Jacob Cedar <jac2552.rit.edu>
2020-10-20 17:37:42 -0400

    implements symbolic link

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import os
 
 def main():
     subprocess.call('clear')
-    while(True):
+    while(y= True):
         subprocess.call(['echo', 'Your current directory is:'])
         subprocess.call('pwd')
         x = input("What file would you like to create a shortcut for?")
-        print(x)
+        if(x.equals('quit')):
+            y= False
+        if(os.path.isfile(x)):
+            subprocess.call(['ln','-s',x])
+        else:
+            pass
+        subprocess.call(['echo', 'The following links exist'])
+        subprocess.call(['ls', '-l', ~/Desktop])
 main()
